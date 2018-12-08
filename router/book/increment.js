767ca76614db141ba3f0f0cdfbb2862f28bacc2b
leiquan <leiquan@live.com>
2018-12-08 19:05:25 +0800

    优化自增

--- router/book/increment.js
+++ router/book/increment.js
@@ -10,7 +10,10 @@ router.post('/', async function (ctx, next) {
   let get = ctx.request.query;
   let post = ctx.request.body;
 
-  let data = await dao.update(post, get);
+  let cloum = get.cloum ? get.cloum : '';   // cloum 可以是多个数组字符串
+  let by = get.by ? get.by : 1;
+
+  let data = await dao.increment([cloum], post, by);
 
   ctx.body = {
     code: 0,
