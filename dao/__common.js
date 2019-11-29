913e35abb6ffe113510197bc8e96c8ce8fe41e37
leiquan <leiquan@live.com>
2019-11-30 03:03:02 +0800

    update

---
+++ dao/__common.js
@@ -0,0 +1,78 @@
+let Sequelize = require('sequelize');
+let path = require('path');
+let env = require('../config/env');
+
+module.exports = function (model) {
+
+    // 这里是自定义Dao方法
+    return {
+
+        add: async function (addJson) {
+            let data = await model.create(addJson,
+                {
+                    logging: env.logging,
+                }
+            );
+            return data;
+        },
+
+        delete: async function (id = null) {
+            let data = await model.destroy({
+                where: {
+                    id: id
+                },
+                logging: env.logging,
+            });
+            return data;
+        },
+
+        update: async function (updateJson = {}, whereJson = {}) {
+            let data = await model.update(updateJson, {
+                where: whereJson,
+                logging: env.logging,
+            });
+            return data;
+        },
+
+        // list: async function (whereJson = {}, page = 1, pageSize = 10) {
+        //     let data = await model.findAndCountAll({
+        //         logging: env.logging,
+        //         where: whereJson,
+        //         offset: pageSize * (page - 1),
+        //         limit: pageSize
+        //     });
+        //     return data;
+        // },
+
+        all: async function (whereJson = {}) {
+            let data = await model.findAndCountAll({
+                logging: env.logging,
+                where: whereJson
+            });
+            return data;
+        },
+
+        sum: async function (cloum, whereJson) {
+            let data = await model.sum(cloum, {
+                where: whereJson
+            });
+            return data;
+        },
+
+        count: async function () {
+            let data = await model.count({
+                logging: env.logging
+            });
+            return data;
+        },
+
+        increment: async function (cloumArray = [], whereJson = {}, by = 1) {
+            let data = await model.increment(cloumArray, {
+                by: by,
+                where: whereJson
+            });
+            return data;
+        }
+    };
+
+};
