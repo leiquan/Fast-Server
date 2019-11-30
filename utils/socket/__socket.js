91857f3fcf26593436b8d4d7d024460dda580484
leiquan <leiquan@live.com>
2019-12-01 02:06:44 +0800

    优化 socket会话池的管理

--- utils/socket/__socket.js
+++ utils/socket/__socket.js
@@ -3,40 +3,22 @@ let io = require("socket.io");
 // 用数组保存全部socket会话，以供使用
 var allClients = [];
 
-module.exports = function (server) {
+// 此函数不能被多次调用，否则会话数量将出现错误
+module.exports = function(server) {
   let socket = io(server);
 
+  // 通用处理，管理会话池
   socket.on("connection", client => {
     allClients.push(client);
-
-    console.log(
-      "Web Socket新增1个连接：",
-      client.id,
-      "，目前连接数量：",
-      allClients.length
-    );
-
-    client.on("send", data => {
-      console.log(data);
-    });
 
     client.on("disconnect", reason => {
       var i = allClients.indexOf(client);
       allClients.splice(i, 1);
-      console.log(
-        "Web Socket断开1个连接：",
-        client.id,
-        "，理由：",
-        reason,
-        "，目前连接数量：",
-        allClients.length
-      );
+    });
+
+    // 自定义处理方法
+    client.on("send", data => {
+      console.log(data);
     });
   });
-
-  return {
-    socket,
-    allClients
-  }
-
 };
